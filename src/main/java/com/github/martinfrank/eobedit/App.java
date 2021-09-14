d3de68d0e10c7cdf1d21c2126f25495544f0deb1
Frank Martin <Martin.Frank@finnova.com>
2021-09-14 11:22:59 +0200

    initial setup

---
+++ src/main/java/com/github/martinfrank/eobedit/App.java
@@ -0,0 +1,13 @@
+package com.github.martinfrank.eobedit;
+
+public class App {
+
+    public static void main(String[] args){
+        App app = new App();
+        app.start();
+    }
+
+    private void start() {
+    }
+
+}
---
+++ src/test/java/com/github/martinfrank/eobedit/AppTest.java
@@ -0,0 +1,13 @@
+package com.github.martinfrank.eobedit;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class AppTest {
+
+    @Test
+    public void simpleTest(){
+        //rigorous test
+        Assert.assertTrue(true);
+    }
+}
